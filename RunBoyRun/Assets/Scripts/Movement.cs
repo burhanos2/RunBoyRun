60a145c0130f3429b8d3ca292704a6f4593866c2
Burhanos2 <24742@ma-web.nl>
2018-09-26 12:32:43 +0200

    basic jump added, needs ground check

--- RunBoyRun/Assets/Scripts/Movement.cs
+++ RunBoyRun/Assets/Scripts/Movement.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour {
+    Rigidbody2D player;
+    public float _jumpspeed;
+    public float AirtimeMax;
+    float _airtime;
+
+    bool grounded;
 
 	// Use this for initialization
 	void Start () {
-		
+        player = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        //player.transform.position(0, 20, 0);
+//jump
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _airtime--;
+            if(_airtime > 0) { player.AddForce(player.transform.up * _jumpspeed, ForceMode2D.Impulse); }
+        }
+        if(Input.GetKeyUp(KeyCode.Space))
+        { _airtime = AirtimeMax; }
 	}
 }
